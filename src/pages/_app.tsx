662c1e614adf77e45824c9d43279eca049fee4cc
Akash Singh <developer.akash8@gmail.com>
2023-10-03 15:43:36 +0600

    update: favicon, title & description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Qanst</title>
+        <meta
+          name="description"
+          content="Empowering Curiosity, Connecting Minds. Create insightful forms, spark meaningful conversations, and explore a world of knowledge. Ask, Answer, Learn – Your platform for collaborative inquiry and shared wisdom"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
