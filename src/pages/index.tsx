4d43afb679751106722c3becb3f30e7ba49b4c9e
Akash Singh <developer.akash8@gmail.com>
2023-10-03 23:57:27 +0600

    index page: added content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import style from "@/styles/Home.module.css";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className={style.home}>
+      <div className={style.detail}>
+        <h1>Transform Questions into Conversations</h1>
+        <p>
+          Your hub for collaborative questioning,
+          <br />
+          form creation, and shared insights.
+        </p>
+
+        <button className="button">
+          Get started <span>&#10230;</span>
+        </button>
+      </div>
+
+      <img src="/home.png" alt="home" />
     </main>
   );
 }
