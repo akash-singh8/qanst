b5bf4bb5d29ab9208a4a201f05f18377feadd4fb
Akash Singh <developer.akash8@gmail.com>
2023-10-04 01:21:08 +0600

    added recoil

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import { RecoilRoot } from "recoil";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
@@ -7,20 +8,22 @@ import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Head>
-        <title>Qanst</title>
-        <meta
-          name="description"
-          content="Empowering Curiosity, Connecting Minds. Create insightful forms, spark meaningful conversations, and explore a world of knowledge. Ask, Answer, Learn – Your platform for collaborative inquiry and shared wisdom"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <RecoilRoot>
+      <SessionProvider session={pageProps.session}>
+        <Head>
+          <title>Qanst</title>
+          <meta
+            name="description"
+            content="Empowering Curiosity, Connecting Minds. Create insightful forms, spark meaningful conversations, and explore a world of knowledge. Ask, Answer, Learn – Your platform for collaborative inquiry and shared wisdom"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-    </SessionProvider>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </SessionProvider>
+    </RecoilRoot>
   );
 }
