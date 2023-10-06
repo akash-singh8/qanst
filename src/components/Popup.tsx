452fa431276c091e31241f8d4f0a3a1e8f18550f
Akash Singh <developer.akash8@gmail.com>
2023-10-06 14:52:12 +0600

    popup: create qna & copy link

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -23,9 +23,53 @@ const Popup = () => {
     cross?.addEventListener("click", clickHandler);
   }, []);
 
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const title = document.querySelector(
+      `.${style.container} input`
+    ) as HTMLInputElement;
+    const button = document.querySelector(
+      `.${style.container} button`
+    ) as HTMLButtonElement;
+
+    try {
+      button.disabled = true;
+      button.innerText = "Creating";
+
+      const loadingId = setInterval(() => {
+        button.innerText += ".";
+        if (button.innerText.length > 12) {
+          button.innerText = "Creating";
+        }
+      }, 250);
+
+      const response = await fetch("http://localhost:3000/api/qna", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: title?.value,
+          email: "developer.akash8@gmail.com",
+        }),
+      });
+
+      const data = await response.json();
+
+      clearInterval(loadingId);
+      title.disabled = true;
+      title.value = `http://localhost:3000/qna/${data.formId}`;
+      navigator.clipboard.writeText(title.value);
+      button.innerText = "Link copied!";
+    } catch (err) {
+      alert("Error while hosing QnA!\nPlease try later.");
+    }
+  };
+
   return (
     <div className={style.popup}>
-      <div className={style.container}>
+      <form className={style.container} onSubmit={handleSubmit}>
         <img src="/form.png" alt="form" className={style.form} />
 
         <input
@@ -37,7 +81,7 @@ const Popup = () => {
         <button className="button">Create QnA</button>
 
         <img src="/cross.svg" alt="cross" className={style.cross} />
-      </div>
+      </form>
     </div>
   );
 };
