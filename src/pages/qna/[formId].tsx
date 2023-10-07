ac69f3d05ad1657fe613108f84d4bbb329600d20
Akash Singh <developer.akash8@gmail.com>
2023-10-07 16:55:24 +0600

    qna: handle bad request

--- src/pages/qna/[formId].tsx
+++ src/pages/qna/[formId].tsx
@@ -12,8 +12,12 @@ const Form = () => {
     async function getForm() {
       const form = await fetch(`http://localhost:3000/api/qna?fid=${formId}`);
 
-      const data = await form.json();
+      if (form.status !== 200) {
+        setForm(400);
+        return;
+      }
 
+      const data = await form.json();
       setForm(data);
     }
 
@@ -23,35 +27,41 @@ const Form = () => {
   return (
     <main className={style.form}>
       {form ? (
-        <>
-          <div className={style.head}>
-            <img src={form?.host?.pictureUrl} alt="user" />
-            <div className={style.detail}>
-              <p>{new Date(form?.createdAt).toDateString()}</p>
-              <h1>{form?.title}</h1>
+        form !== 400 ? (
+          <>
+            <div className={style.head}>
+              <img src={form?.host?.pictureUrl} alt="user" />
+              <div className={style.detail}>
+                <p>{new Date(form?.createdAt).toDateString()}</p>
+                <h1>{form?.title}</h1>
+              </div>
             </div>
-          </div>
 
-          <div>
-            {form?.questions?.map((que: any) => (
-              <Question
-                content={que.content}
-                user={que.user}
-                answers={que.answers}
-                date={que.createdAt}
-                votes={que.votes.length}
-              />
-            ))}
-          </div>
-
-          <div className={style.input_box}>
-            <div className={style.input}>
-              <img src="/question.svg" alt="ask question" />
-              <input type="text" placeholder="Ask a question" />
-              <button>post</button>
+            <div>
+              {form?.questions?.map((que: any) => (
+                <Question
+                  content={que.content}
+                  user={que.user}
+                  answers={que.answers}
+                  date={que.createdAt}
+                  votes={que.votes.length}
+                />
+              ))}
             </div>
+
+            <div className={style.input_box}>
+              <div className={style.input}>
+                <img src="/question.svg" alt="ask question" />
+                <input type="text" placeholder="Ask a question" />
+                <button>post</button>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className={style.badrequest}>
+            <img src="/badrequest.svg" alt="bad request" />
           </div>
-        </>
+        )
       ) : (
         <div className={style.loading}>
           <img src="/loading.svg" alt="loading" />
