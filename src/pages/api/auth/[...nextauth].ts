b809e6bcc58061341d2cd528796f0dc4070adddb
Akash Singh <developer.akash8@gmail.com>
2023-10-08 15:29:56 +0600

    next-auth: added signIn callback - store user data

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth";
+import NextAuth, { User } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "../qna";
 
 export const authOptions = {
   providers: [
@@ -8,6 +9,25 @@ export const authOptions = {
       clientSecret: process.env.NEXT_AUTH_CLIENT_SECRET!,
     }),
   ],
+  callbacks: {
+    async signIn({ user }: { user: User }) {
+      try {
+        const newUser = await prisma.user.create({
+          data: {
+            name: user.name as string,
+            email: user.email as string,
+            pictureUrl: user.image as string,
+          },
+        });
+
+        console.log("New user added :", newUser);
+      } catch (err) {
+        console.log("User already added");
+      }
+
+      return user;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
