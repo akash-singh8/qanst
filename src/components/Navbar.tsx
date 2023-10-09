9de03a7821ce45c6b7f707220abd0a2d2c7b63e2
Akash Singh <developer.akash8@gmail.com>
2023-10-09 12:59:08 +0600

    nav: added logout popup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import authState from "@/recoil/auth";
 import userState from "@/recoil/user";
 import { useSetRecoilState } from "recoil";
 import Link from "next/link";
+import { useEffect } from "react";
 
 const Navbar = () => {
   const session = useSession();
@@ -16,19 +17,63 @@ const Navbar = () => {
     setUser(session.data.user);
   }
 
+  useEffect(() => {
+    const userButton = document.querySelector(`.${style.user}`);
+    const popup = document.querySelector(`.${style.popup}`) as HTMLDivElement;
+
+    function hidePopup() {
+      popup.style.top = "100%";
+      popup.style.opacity = "0";
+      setTimeout(() => {
+        popup.style.display = "none";
+      }, 250);
+
+      document.removeEventListener("click", hidePopup);
+      userButton?.addEventListener("click", showPopup);
+    }
+
+    function showPopup() {
+      popup.style.display = "block";
+      setTimeout(() => {
+        popup.style.top = "85%";
+        popup.style.opacity = "1";
+      }, 10);
+
+      userButton?.removeEventListener("click", showPopup);
+
+      setTimeout(() => {
+        document.addEventListener("click", hidePopup);
+      }, 10);
+    }
+
+    userButton?.addEventListener("click", showPopup);
+  }, [session.data]);
+
   return (
     <nav className={style.nav}>
       <Link href={"/"}>
         <img src="/logo.png" alt="Qanst" />
       </Link>
       {session.data ? (
-        <button
-          className="button"
-          onClick={() => {
-            signOut();
-          }}>
-          Logout
-        </button>
+        <div className={style.logged_user}>
+          <button className={`${style.user} button`}>
+            <span>{session.data.user?.name?.split(" ")[0]}</span>
+            <img src={session.data.user?.image as string} alt="user" />
+          </button>
+
+          <div className={style.popup}>
+            <p>Hello,</p>
+            <strong>{session.data.user?.name?.split(" ")[0]}</strong>
+
+            <button
+              className="button"
+              onClick={() => {
+                signOut();
+              }}>
+              Logout
+            </button>
+          </div>
+        </div>
       ) : (
         <button
           className="button"
