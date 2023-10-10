d9aacab00b2e5d8137cf632a4b6e75f1033d964d
Akash Singh <developer.akash8@gmail.com>
2023-10-10 10:58:34 +0600

    qna: render on server-side

--- src/pages/qna/[formId].tsx
+++ src/pages/qna/[formId].tsx
@@ -1,32 +1,34 @@
-import { useRouter } from "next/router";
 import style from "@/styles/Form.module.css";
 import Question from "@/components/Question";
-import { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import userState from "@/recoil/user";
 import View from "@/components/View";
 
-const Form = () => {
+export async function getServerSideProps(context: any) {
+  const formId = context.resolvedUrl.slice(5) as string;
+
+  const response = await fetch(`http://localhost:3000/api/qna?fid=${formId}`);
+
+  if (!response.ok) {
+    return {
+      props: {
+        form: null,
+      },
+    };
+  }
+
+  const data = await response.json();
+
+  return {
+    props: {
+      formId,
+      form: data,
+    },
+  };
+}
+
+const Form = ({ formId, form }: { formId: string; form: any }) => {
   const user = useRecoilValue(userState);
-  const router = useRouter();
-  const formId = router.query.formId;
-  const [form, setForm] = useState<any>();
-
-  useEffect(() => {
-    async function getForm() {
-      const form = await fetch(`http://localhost:3000/api/qna?fid=${formId}`);
-
-      if (form.status !== 200) {
-        setForm(400);
-        return;
-      }
-
-      const data = await form.json();
-      setForm(data);
-    }
-
-    if (formId) getForm();
-  }, [formId]);
 
   const postQuestion = async () => {
     const queElement = document.querySelector(
@@ -71,46 +73,40 @@ const Form = () => {
   return (
     <main className={style.form}>
       {form ? (
-        form !== 400 ? (
-          <>
-            <div className={style.head}>
-              <View user={form.host} createdAt={form.createdAt} />
-              <div className={style.detail}>
-                <p>{new Date(form.createdAt).toDateString()}</p>
-                <h1>{form.title}</h1>
-              </div>
+        <>
+          <div className={style.head}>
+            <View user={form.host} createdAt={form.createdAt} />
+            <div className={style.detail}>
+              <p>{new Date(form.createdAt).toDateString()}</p>
+              <h1>{form.title}</h1>
             </div>
-
-            <div>
-              {form.questions.map((que: any) => (
-                <Question
-                  key={que.qid}
-                  id={que.qid}
-                  content={que.content}
-                  user={que.user}
-                  answers={que.answers}
-                  date={que.createdAt}
-                  votes={que.votes}
-                />
-              ))}
-            </div>
-
-            <div className={style.input_box}>
-              <div className={style.input}>
-                <img src="/question.svg" alt="ask question" />
-                <input type="text" placeholder="Ask a question" />
-                <button onClick={postQuestion}>post</button>
-              </div>
-            </div>
-          </>
-        ) : (
-          <div className={style.badrequest}>
-            <img src="/badrequest.svg" alt="bad request" />
           </div>
-        )
+
+          <div>
+            {form.questions.map((que: any) => (
+              <Question
+                key={que.qid}
+                id={que.qid}
+                content={que.content}
+                user={que.user}
+                answers={que.answers}
+                date={que.createdAt}
+                votes={que.votes}
+              />
+            ))}
+          </div>
+
+          <div className={style.input_box}>
+            <div className={style.input}>
+              <img src="/question.svg" alt="ask question" />
+              <input type="text" placeholder="Ask a question" />
+              <button onClick={postQuestion}>post</button>
+            </div>
+          </div>
+        </>
       ) : (
-        <div className={style.loading}>
-          <img src="/loading.svg" alt="loading" />
+        <div className={style.badrequest}>
+          <img src="/badrequest.svg" alt="bad request" />
         </div>
       )}
     </main>
