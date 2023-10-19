112ff6e2726d59e9db2dd9e8955218c7003abbc8
Akash Singh <developer.akash8@gmail.com>
2023-10-20 00:38:14 +0600

    fix: Title typo

--- src/validation/form.ts
+++ src/validation/form.ts
@@ -8,8 +8,8 @@ const validateForm = z.object({
     .email("Invalid email address"),
   title: z
     .string()
-    .min(5, "Email must be at least 5 characters long")
-    .max(200, "Email must be at most 200 characters long"),
+    .min(5, "Title must be at least 5 characters long")
+    .max(200, "Title must be at most 200 characters long"),
 });
 
 export default validateForm;
